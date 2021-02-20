437626a1623023a6ec6ba819445531614a932b50
Jose Francisco Flores Huere <jfloresh631@gmail.com>
2021-02-19 21:04:52 -0500

    Resolviendo problemas en los links

--- scripts/api.js
+++ scripts/api.js
@@ -1,66 +1,47 @@
 const $titles = document.querySelectorAll('.article__title a');
 const $images = document.querySelectorAll('.image');
 const $introductions = document.querySelectorAll('.article__introduction');
+const $links = document.querySelectorAll('.article__link');
 const $tops = document.querySelectorAll('.tops a');
 const $recents = document.querySelectorAll('.recents a');
-const $link = document.querySelectorAll('.article__link');
-const $titleLink = document.querySelectorAll('.article--big__title')
-
-
 
 export default function getNews() {
-    
     const apiKey = 'qT1M4S2fPd1JSAQF0AZL3sg36dqtC1aw';
 
     const categories = {
         technology: 'technology',
-        science: 'science',
+        health: 'health',
         arts: 'arts'
-
     }
 
-    const {technology, science, arts} = categories;
-    
+    const { technology, health, arts } = categories;
 
-    const Articles = (n, result) => {
-        
-        $titles[n].textContent = result.results[0].title;
-        $images[n].setAttribute('src', `${result.results[0].multimedia[2].url}`);
-        $introductions[n].textContent = result.results[0].abstract;
-        
+    const drawArticles = (index, result) => {
+        $titles[index].innerText = result.results[0].title;
+        $titles[index].setAttribute('href', `${result.results[0].url}`);
+        $images[index].setAttribute('src', `${result.results[0].multimedia[2].url}`);
+        $introductions[index].innerText = result.results[0].abstract;
+        $links[index].setAttribute('href', `${result.results[0].url}`);
     }
 
-    const rightColumn = (n,n2, n3, result) => {
-        $tops[n].textContent = result.results[n2].title;
-        $recents[n].textContent = result.results[n3].title;
-        $tops[n].setAttribute('href', `${result.results[n2].url}`);
-        $recents[n].setAttribute('href', `${result.results[n3].url}`);
-        
-        
+    const drawLinks = (index, result) => {
+        $tops[index].innerText = result.results[index + 1].title;
+        $tops[index].setAttribute('href', `${result.results[index + 1].url}`);
+        $recents[index].innerText = result.results[index + 2].title;
+        $recents[index].setAttribute('href', `${result.results[index + 2].url}`);
     }
 
-    const Links = (n,result) => {
-        
-        $link[n].setAttribute('href', `${result.results[0].url}`);
-        $titleLink[n].setAttribute('href', `${result.results[0].url}`);    
-         
-    }
-
-    
-    const News = async (category, ArticleNum,rC1,rC2,rC3,LinkNum) => {
-        
+    const loadNews = async (category, article, index) => {
         const api = await fetch(`https://api.nytimes.com/svc/news/v3/content/all/${category}.json?limit=6&api-key=${apiKey}`);
+
         const result = await api.json();
-        
-        Articles(ArticleNum,result);
-        rightColumn(rC1,rC2,rC3,result);
-        Links(LinkNum,result);
+        console.log(result)
 
+        drawArticles(article, result);
+        drawLinks(index, result);
     }
-    
 
-    News(technology,0,1,2,3,0);
-    News(science,1,2,3,4,1)
-    News(arts,2,3,4,5,2);
-    
+    loadNews(technology, 0, 0);
+    loadNews(health, 1, 1);
+    loadNews(arts, 2, 2);
 }
--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,11 @@
-
 import toggleMenu from "./menu.js";
 import toggleSearch from "./search.js";
 import getNews from './api.js';
  
 function main () {
-    
     toggleMenu();
     toggleSearch();
     getNews();
-
-   
-   
 }
 
 document.addEventListener('DOMContentLoaded', main)
